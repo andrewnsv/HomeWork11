3ec3e326aa9efe4b84a9795c54c368599549dc1a
andrewnsv <andrejnizovoj598@gmail.com>
2023-01-11 22:14:30 +0200

    fix styles and add refresh function

--- main.js
+++ main.js
@@ -1,12 +1,5 @@
 const output = document.getElementById("output");
-const outSideClick = document.getElementsByTagName("body");
-const result = document.querySelector(".res span");
-const restoreBtn = document.getElementById("restoreBtn");
-const removedLis = [];
-
-restoreBtn.onClick = () => {
-    parent.location.reload();
-}
+const restoreBtn = document.getElementById("restore-btn");
 
 fetch("https://rickandmortyapi.com/api/character")
   .then((response) => response.json())
@@ -41,7 +34,7 @@ fetch("https://rickandmortyapi.com/api/character")
         target.classList.add("active");
 
         const res = document.querySelector(".res span");
-        res.textContent = target.querySelector("p").textContent;
+        res.textContent = target.querySelectorAll("p")[0].textContent;
 
         if (e.target.matches('button[btn-name="delete"]')) {
           target.remove();
@@ -49,4 +42,33 @@ fetch("https://rickandmortyapi.com/api/character")
         }
       }
     });
+
+
+    restoreBtn.addEventListener('click', () => {
+      const listItems = document.querySelectorAll("li");
+
+      if (listItems.length > 0) {
+        listItems.forEach(item => {
+          item.remove();
+        });
+      }
+
+      data.results.forEach((d) => {
+        output.innerHTML += `<li>
+          <img src="${d.image}">
+              <div class="wrap-item">
+                  <div class='name-content'>Имя:
+                      <p>${d.name}</p>
+                  </div>
+                  <div class='name-content'>Статус:
+                      <p> ${d.status}</p>
+                  </div>
+                  <div class='name-content'>Айди: 
+                      <p>${d.id}</p>
+                  </div>
+                  <button btn-name="delete">Delete Block</button>
+              </div>
+          </li>`;
+      });
+    })
   });
